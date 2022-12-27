1e8e864333c97d783132808b8ee3e53f20a37ea8
Ганеев Рустам <ganeevrm@yahoo.com>
2022-12-27 22:26:43 +0300

    + Завершил Главу 1
    Добавил уведомления и назначил слушателей для кнопок

--- GeoQuiz/app/src/main/java/com/ganeevrm/android/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/ganeevrm/android/geoquiz/MainActivity.kt
@@ -2,10 +2,28 @@ package com.ganeevrm.android.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener {
+            val toast = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.TOP,0, 0)
+            toast.show()
+        }
+
+        falseButton.setOnClickListener {
+            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+        }
     }
 }

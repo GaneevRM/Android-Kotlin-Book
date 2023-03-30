ad83d0bd1808545ad2a3e75026211f88e1a98778
Rustam Ganeev <ganeevrm@yahoo.com>
2023-03-30 09:22:01 +0300

    + Добавил логику для кнопки "Prev"

--- GeoQuiz/app/src/main/java/com/ganeevrm/android/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/ganeevrm/android/geoquiz/MainActivity.kt
@@ -1,16 +1,17 @@
 package com.ganeevrm.android.geoquiz
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var prevButton: ImageButton
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -31,6 +32,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        prevButton = findViewById(R.id.prev_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener {
@@ -43,6 +45,12 @@ class MainActivity : AppCompatActivity() {
 
         nextButton.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        prevButton.setOnClickListener{
+            val diff = currentIndex - 1
+            currentIndex = if (diff == -1) questionBank.size-1 else diff % questionBank.size
             updateQuestion()
         }
 

4779aa9e7a74eaf333e6d4c3c777c2b1356ee751
Rustam Ganeev <ganeevrm@yahoo.com>
2023-04-06 13:20:31 +0300

    + Добавил лог для просмотра состояния активити

--- GeoQuiz/app/src/main/java/com/ganeevrm/android/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/ganeevrm/android/geoquiz/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.ganeevrm.android.geoquiz
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
@@ -27,6 +29,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate")
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
@@ -55,6 +58,26 @@ class MainActivity : AppCompatActivity() {
         }
 
         updateQuestion()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
     }
 
     private fun updateQuestion(){

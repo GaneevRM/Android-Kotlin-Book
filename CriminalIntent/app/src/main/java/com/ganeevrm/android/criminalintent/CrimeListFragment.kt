04947fb9387ef3da3c42374b0552c0a7f412eb42
Rustam Ganeev <ganeevrm@yahoo.com>
2023-08-23 11:13:07 +0300

    + Завершил Главу 10 и выполнил задачу
    - Используйте функции класса DateFormat для формирования строки в стандартном формате или же подготовьте собственную форматную строку.

---
+++ CriminalIntent/app/src/main/java/com/ganeevrm/android/criminalintent/CrimeListFragment.kt
@@ -0,0 +1,108 @@
+package com.ganeevrm.android.criminalintent
+
+import android.os.Bundle
+import android.text.format.DateFormat
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+private const val TAG = "CrimeListFragment"
+
+class CrimeListFragment : Fragment() {
+
+    private lateinit var crimeRecyclerView: RecyclerView
+    private val crimeListViewModel: CrimeListViewModel by lazy {
+        ViewModelProvider(this).get(CrimeListViewModel::class.java)
+    }
+    private var adapter: CrimeAdapter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime_list, container, false)
+        crimeRecyclerView = view.findViewById(R.id.crime_recycler_view) as RecyclerView
+        crimeRecyclerView.layoutManager = LinearLayoutManager(context)
+        updateUI()
+        return view
+    }
+
+    private fun updateUI(){
+        val crimes =  crimeListViewModel.crimes
+        adapter = CrimeAdapter(crimes)
+        crimeRecyclerView.adapter = adapter
+    }
+
+    private inner class CrimeHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
+
+        private lateinit var crime: Crime
+
+        private val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
+        private val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+        private val solvedImageView: ImageView = itemView.findViewById(R.id.crime_solved)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        fun bind(crime: Crime) {
+            this.crime = crime
+            titleTextView.text = this.crime.title
+            dateTextView.text = DateFormat.format("EEE, MMM, dd, yyyy", this.crime.date)
+            solvedImageView.visibility = if(this.crime.isSolved){
+                View.VISIBLE
+            } else {
+                View.GONE
+            }
+        }
+
+        override fun onClick(p0: View?) {
+            Toast.makeText(context, "${crime.title} pressed!", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private inner class CrimeAdapter(var crimes: List<Crime>) : RecyclerView.Adapter<CrimeHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
+            val view = if(viewType==0){
+                layoutInflater.inflate(R.layout.list_item_crime, parent, false)
+            } else {
+                layoutInflater.inflate(R.layout.list_item_crime_police, parent, false)
+            }
+            return CrimeHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return crimes.size
+        }
+
+        override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
+            val crime = crimes[position]
+            holder.bind(crime)
+        }
+
+        override fun getItemViewType(position: Int): Int {
+            return crimes[position].requiresPolice
+        }
+
+    }
+
+    companion object {
+        fun newInstance(): CrimeListFragment {
+            return CrimeListFragment()
+        }
+    }
+}

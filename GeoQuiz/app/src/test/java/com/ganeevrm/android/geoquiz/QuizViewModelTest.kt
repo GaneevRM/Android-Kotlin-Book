6afcac33caa21d4f0a2c392e712f1d6c9d6898d0
Rustam Ganeev <ganeevrm@yahoo.com>
2023-11-07 13:07:01 +0300

    + Обновил 6 главу
    - Добавил инструментальные тесты.
    - Выполнил задание.

--- GeoQuiz/app/src/test/java/com/ganeevrm/android/geoquiz/QuizViewModelTest.kt
+++ GeoQuiz/app/src/test/java/com/ganeevrm/android/geoquiz/QuizViewModelTest.kt
@@ -1,23 +1,34 @@
 package com.ganeevrm.android.geoquiz
 
 import androidx.lifecycle.SavedStateHandle
+import org.junit.Assert.*
 import org.junit.Test
-import org.junit.Assert.assertEquals
 
 internal class QuizViewModelTest {
     @Test
-    fun providesExpectedQuestionText(){
+    fun providesExpectedQuestionText() {
         val savedStateHandle = SavedStateHandle()
         val quizViewModel = QuizViewModel(savedStateHandle)
         assertEquals(R.string.question_australia, quizViewModel.currentQuestionText)
     }
 
     @Test
-    fun wrapsAroundQuestionBank(){
+    fun wrapsAroundQuestionBank() {
         val savedStateHandle = SavedStateHandle(mapOf(CURRENT_INDEX_KEY to 5))
         val quizViewModel = QuizViewModel(savedStateHandle)
         assertEquals(R.string.question_asia, quizViewModel.currentQuestionText)
         quizViewModel.moveToNext()
         assertEquals(R.string.question_australia, quizViewModel.currentQuestionText)
     }
+
+    @Test
+    fun providesExpectedQuestionAnswer() {
+        val savedStateHandle = SavedStateHandle()
+        val quizViewModel = QuizViewModel(savedStateHandle)
+        assertTrue(quizViewModel.currentQuestionAnswer)
+        quizViewModel.moveToNext()
+        quizViewModel.moveToNext()
+        assertFalse(quizViewModel.currentQuestionAnswer)
+    }
+
 }

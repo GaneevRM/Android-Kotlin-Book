1d1cc1cd8f6f8b4cad8feba3e8539684b2e8198e
Rustam Ganeev <ganeevrm@yahoo.com>
2023-11-15 13:29:53 +0300

    + Обновил 6 главу
    - Добавил binding в CheatActivity.kt

--- GeoQuiz/app/src/main/java/com/ganeevrm/android/geoquiz/CheatActivity.kt
+++ GeoQuiz/app/src/main/java/com/ganeevrm/android/geoquiz/CheatActivity.kt
@@ -4,40 +4,34 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
-import androidx.lifecycle.ViewModelProvider
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.ganeevrm.android.geoquiz.databinding.ActivityCheatBinding
 
 const val EXTRA_ANSWER_SHOWN = "com.ganeevrm.android.geoquiz.answer_shown"
 private const val EXTRA_ANSWER_IS_TURE = "com.ganeevrm.android.geoquiz.answer_is_true"
 private const val TAG = "CheatActivity"
 private const val KEY_IS_CHEATER = "isCheater"
+
 class CheatActivity : AppCompatActivity() {
 
-    private lateinit var answerTextView: TextView
-    private lateinit var showAnswerButton: Button
-    private lateinit var apiLevelTextView: TextView
-    private val cheatViewModel: CheatViewModel by lazy {
-        ViewModelProvider(this).get(CheatViewModel::class.java)
-    }
+    private lateinit var binding: ActivityCheatBinding
+    private val cheatViewModel: CheatViewModel by viewModels()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate")
-        setContentView(R.layout.activity_cheat)
+        binding = ActivityCheatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         cheatViewModel.answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TURE, false)
         cheatViewModel.isCheater = savedInstanceState?.getBoolean(KEY_IS_CHEATER, false) ?: false
 
-        answerTextView = findViewById(R.id.answer_text_view)
-        apiLevelTextView = findViewById(R.id.api_level_text_view)
-        showAnswerButton = findViewById(R.id.show_answer_button)
-        showAnswerButton.setOnClickListener {
+        binding.showAnswerButton.setOnClickListener {
             cheatViewModel.isCheater = true
             updateInterface()
         }
@@ -71,19 +65,19 @@ class CheatActivity : AppCompatActivity() {
         Log.d(TAG, "onDestroy")
     }
 
-    private fun updateInterface(){
+    private fun updateInterface() {
         val apiLevelValue = getString(R.string.api_level) + Build.VERSION.SDK_INT
-        apiLevelTextView.text = apiLevelValue
-        if(cheatViewModel.isCheater){
-            showAnswerButton.visibility = View.GONE
+        binding.apiLevelTextView.text = apiLevelValue
+        if (cheatViewModel.isCheater) {
+            binding.showAnswerButton.visibility = View.GONE
             val answerText = when {
                 cheatViewModel.answerIsTrue -> R.string.true_button
                 else -> R.string.false_button
             }
-            answerTextView.setText(answerText)
+            binding.answerTextView.setText(answerText)
             setAnswerShownResult()
         } else {
-            showAnswerButton.visibility = View.VISIBLE
+            binding.showAnswerButton.visibility = View.VISIBLE
         }
     }
 
@@ -97,7 +91,12 @@ class CheatActivity : AppCompatActivity() {
 
     companion object {
         fun newIntent(packageContext: Context, answerIsTrue: Boolean): Intent {
-            return Intent(packageContext, CheatActivity::class.java).apply { putExtra(EXTRA_ANSWER_IS_TURE, answerIsTrue) }
+            return Intent(packageContext, CheatActivity::class.java).apply {
+                putExtra(
+                    EXTRA_ANSWER_IS_TURE,
+                    answerIsTrue
+                )
+            }
         }
     }
 }

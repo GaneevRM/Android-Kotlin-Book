3f4e1d97e78659e30b75be13f933c5a16e5389d0
Rustam Ganeev <ganeevrm@yahoo.com>
2024-03-27 20:36:13 +0300

    + Глава 13. Пройдены разделы
    - Однонаправленный поток данных

---
+++ CriminalIntent/app/src/main/java/com/ganeevrm/android/criminalintent/CrimeDetailFragment.kt
@@ -0,0 +1,83 @@
+package com.ganeevrm.android.criminalintent
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.navArgs
+import com.ganeevrm.android.criminalintent.databinding.FragmentCrimeBinding
+import kotlinx.coroutines.launch
+
+class CrimeDetailFragment : Fragment() {
+
+    private val args: CrimeFragmentArgs by navArgs()
+    private var _binding: FragmentCrimeBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) {
+            "Cannot access binding because it is null. Is the view visible?"
+        }
+    private val crimeDetailViewModel: CrimeDetailViewModel by viewModels {
+        CrimeDetailViewModelFactory(
+            args.crimeId
+        )
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCrimeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            crimeTitle.doOnTextChanged { text, _, _, _ ->
+                crimeDetailViewModel.updateCrime { oldCrime ->
+                    oldCrime.copy(title = text.toString())
+                }
+            }
+
+            crimeDate.apply {
+                isEnabled = false
+            }
+
+            crimeSolved.setOnCheckedChangeListener { _, isChecked ->
+                crimeDetailViewModel.updateCrime { oldCrime ->
+                    oldCrime.copy(isSolved = isChecked)
+                }
+            }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                crimeDetailViewModel.crime.collect { crime ->
+                    crime?.let { updateUi(it) }
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun updateUi(crime: Crime) {
+        binding.apply {
+            if (crimeTitle.text.toString() != crime.title) {
+                crimeTitle.setText(crime.title)
+            }
+            crimeDate.text = crime.date.toString()
+            crimeSolved.isChecked = crime.isSolved
+        }
+    }
+}
---
+++ CriminalIntent/app/src/main/java/com/ganeevrm/android/criminalintent/CrimeDetailViewModel.kt
@@ -0,0 +1,36 @@
+package com.ganeevrm.android.criminalintent
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import java.util.UUID
+
+class CrimeDetailViewModel(crimeId: UUID) : ViewModel() {
+    private val crimeRepository = CrimeRepository.get()
+
+    private val _crime: MutableStateFlow<Crime?> = MutableStateFlow(null)
+    val crime: StateFlow<Crime?> = _crime.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            _crime.value = crimeRepository.getCrime(crimeId)
+        }
+    }
+
+    fun updateCrime(onUpdate: (Crime) -> Crime) {
+        _crime.update { oldCrime ->
+            oldCrime?.let { onUpdate(it) }
+        }
+    }
+}
+
+class CrimeDetailViewModelFactory(private val crimeId: UUID) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return CrimeDetailViewModel(crimeId) as T
+    }
+}
